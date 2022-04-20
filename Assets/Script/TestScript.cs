6aa2c6b8c3be7ced6a4961c2ee633d26d435d29f
haitaoli <1808464468@qq.com>
2022-04-20 23:35:03 +0800

    add file

---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public Text _Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Text.text = "Hello WebGame";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
