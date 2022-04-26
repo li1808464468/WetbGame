972ed0a2bc1691dab4bf3374d4335ce1ad48f103
haitao.li <haitao.li@blowfires.com>
2022-04-26 23:33:11 +0800

    add Game

--- build-template/android/unityLibrary/src/main/java/h5adapter/broadcast/BroadcastListener.java
+++
@@ -1,9 +0,0 @@
-package h5adapter.broadcast;
-
-import android.content.Context;
-import android.content.Intent;
-
-public interface BroadcastListener {
-
-    void onReceive(Context context, Intent intent);
-}
